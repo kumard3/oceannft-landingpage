aa83b7514f4f4920fd63aa6fd34c5556b8cd8976
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-14 02:02:09 +0530

    new section added

---
+++ components/TopSellers.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function TopSellers() {
+  return (
+    <div>
+      <div className="">
+        <h1 className="font-orbitron">Top Sellers </h1>
+        <p>
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim minim mollit.
+        </p>
+      </div>
+      <div className="">
+      <div></div>
+
+      </div>
+    </div>
+  );
+}
